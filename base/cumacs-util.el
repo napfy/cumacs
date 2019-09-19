f900ca53ac7324b2c25cbb4c62902c080ce6641c
yuefeng <yuef@yuefengdeiMac.local>
2019-09-19 16:53:09 +0800

    增加了Git的快捷访问，修改了菜单对应的快捷键，修复了早期的错误

--- base/cumacs-util.el
+++ base/cumacs-util.el
@@ -171,38 +171,38 @@
 (pretty-hydra-define cumacs-shortcut
   (:quit-key "q" :color pink)
   ("浏览"
-                     (("l" helm-buffers-list "Buffer列表")
-                      ("p" previous-buffer "上一Buffer")
-                      ("n" next-buffer "下一Buffer"))
-                     "搜索"
-                     (("g" rg-project "指定类型正则查找")
-                      ("s" project-find-regexp "指定目录正则查找")
-                      ("m" helm-multi-swoop-projectile "多文件的查找替换"))
-                    
-                     "      窗口切换"
-                     (("j" eyebrowse-next-window-config "下一状态")
-                      ("k" eyebrowse-prev-window-config "上一状态")
-                      ("t" eyebrowse-switch-to-window-config "指定配置"))
-                     "     窗口状态"
-                     (("w" eyebrowse-create-window-config "保存状态")
-                      ("r" eyebrowse-close-window-config "移除状态")
-                      ("x" delete-window "关闭窗口"))
-                     "    窗口大小"
-                     ((">" enlarge-window-horizontally "横向扩展")
-                      ("<" shrink-window-horizontally "横向缩小")
-                      ("h" split-window-horizontally "横向分隔"))
-                     "    代码折叠"
-                     (("," hs-hide-all "折叠全部")
-                      ("." hs-show-all "展开全部")
-                      ("/" hs-toggle-hiding "折叠当前"))
-                      "    开发工具"
-                     (("c" eshell "命令行")
-                      ("e" restclient-mode "Rest客户端"))
-                     "   实用功能"
-                     (("b" browse-web "浏览网址")
-                      ("o" org-capture "备忘录")
-                      )
-                     ))
+   (("l" helm-buffers-list "Buffer列表")
+    ("p" previous-buffer "上一Buffer")
+    ("n" next-buffer "下一Buffer"))
+   "搜索"
+   (("r" rg-project "指定类型正则查找")
+    ("s" project-find-regexp "指定目录正则查找")
+    ("m" helm-multi-swoop-projectile "多文件的查找替换"))
+   
+   "      窗口切换"
+   (("j" eyebrowse-next-window-config "下一状态")
+    ("k" eyebrowse-prev-window-config "上一状态")
+    ("t" eyebrowse-switch-to-window-config "指定配置"))
+   "     窗口状态"
+   (("w" eyebrowse-create-window-config "保存状态")
+    ("d" eyebrowse-close-window-config "移除状态")
+    ("x" delete-window "关闭窗口"))
+   "    窗口大小"
+   ((">" enlarge-window-horizontally "横向扩展")
+    ("<" shrink-window-horizontally "横向缩小")
+    ("h" split-window-horizontally "横向分隔"))
+   "    代码折叠"
+   (("," hs-hide-all "折叠全部")
+    ("." hs-show-all "展开全部")
+    ("/" hs-toggle-hiding "折叠当前"))
+   "    开发工具"
+   (("c" eshell "命令行")
+    ("e" restclient-mode "Rest客户端")
+    ("g" magit-status "Git客户端"))
+   "   实用功能"
+   (("b" browse-web "浏览网址")
+    ("o" org-capture "备忘录"))
+   ))
 
 (global-set-key (kbd "<C-escape>") 'cumacs-shortcut/body)
 
